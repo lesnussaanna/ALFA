f6484922206d749aa0a81ce33d5ab674fbf07cb7
Nanda Amelia <nanda.amelia1004@gmail.com>
2022-11-22 22:02:15 +0700

    admin : layout notifikasi dan search admin

---
+++ app/src/main/java/com/lab/alfa/admin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lab.alfa.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lab.alfa.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notifikasi)
+    }
+}
