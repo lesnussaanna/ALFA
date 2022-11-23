cedb4dcea1d91dcaaa029b215f26384ec9528137
lesnussaanna <kettykhaterine@gmail.com>
2022-11-23 17:20:35 +0700

    pengajuan mahasiswa dan tampilan chat dengan recyclerview

---
+++ app/src/main/java/com/lab/alfa/admin/PengajuanAlumni.kt
@@ -0,0 +1,12 @@
+package com.lab.alfa.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lab.alfa.R
+
+class PengajuanAlumni : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pengajuan_alumni)
+    }
+}
---
+++ app/src/main/java/com/lab/alfa/alumni/ui/ChatAlumni.kt
@@ -0,0 +1,12 @@
+package com.lab.alfa.alumni.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lab.alfa.R
+
+class ChatAlumni : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_alumni)
+    }
+}
