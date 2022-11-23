9e61757eedeca682faee84e2b22744e2a167d720
lesnussaanna <kettykhaterine@gmail.com>
2022-11-23 14:52:32 +0700

    Layout Sign In dan SplashScreen

---
+++ app/src/main/java/com/lab/alfa/SignIn.kt
@@ -0,0 +1,17 @@
+package com.lab.alfa
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+
+class SignIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+    }
+}
