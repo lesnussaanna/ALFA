e66ff432f6a590755e7eb172e97f07b209236546
Nanda Amelia <nanda.amelia1004@gmail.com>
2022-12-06 11:37:26 +0700

    alumni : home alumni menu

---
+++ app/src/main/java/com/lab/alfa/admin/ChatAlumniActivity.kt
@@ -0,0 +1,11 @@
+package com.lab.alfa.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatAlumniActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_alumni)
+    }
+}
