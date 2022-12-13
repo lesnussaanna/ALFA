83569f68c2c825d1ca2c41c86c254ac7e5d97502
Nanda Amelia <nanda.amelia1004@gmail.com>
2022-12-13 14:45:02 +0700

    home admin

---
+++ app/src/main/java/com/lab/alfa/admin/HomeAlumniMenuActivity.kt
@@ -0,0 +1,12 @@
+package com.lab.alfa.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lab.alfa.R
+
+class HomeAlumniMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_alumni_menu)
+    }
+}
